45a31d5f4ea8366736db8dbdc81ffc9ec77e0b48
mortadhabennaceur <mortadha.bennaceur@esprit.tn>
2025-04-15 18:31:59 +0100

    Mortadha + Amine + Elyes

--- src/main/java/com/esprit/pi/configs/JacksonConfig.java
+++ src/main/java/com/esprit/pi/configs/JacksonConfig.java
@@ -3,6 +3,7 @@ package com.esprit.pi.configs;
 import com.fasterxml.jackson.core.StreamWriteConstraints;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,8 @@ public class JacksonConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new JavaTimeModule()); // 👈 Required for LocalDateTime
 
 
         // Customize the max nesting depth
